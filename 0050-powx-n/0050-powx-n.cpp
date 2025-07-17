ebe5c0f642289c4851cc3e52a79eb15c8679f835
Talha Jubaer Prantor <talhajubaer3121@gmail.com>
2025-07-17 19:20:18 +0600

    Time: 0 ms (100%), Space: 8.6 MB (42.29%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double helper(double x, long n){
+        if(n == 0) return 1;
+        if(x == 0) return 0;
+
+        double res = helper(x, n / 2);
+        res = res * res;
+
+        return (n & 1) ? res * x : res;
+    }
+
+    double myPow(double x, int n) {
+        long N = n;
+        double ans = helper(x, abs(N));
+        return (n >= 0) ? ans : 1 / ans;
+    }
+};
